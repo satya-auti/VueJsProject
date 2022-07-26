85ec835eec479d66b13602bcbb5853e77ebb56be
satyawan auti <auti.satyavan3@gmail.com>
2022-07-26 11:47:54 +0530

    conflicts

---
+++ src/components/child.vue
@@ -0,0 +1,19 @@
+<template>
+
+<inner-child></inner-child>
+    <h1>Hello I'm child component of user_registration</h1>
+    <h1>User Registration Form </h1>
+</template>
+<script>
+import InnerChild from './innerChild.vue';
+    export default {
+        name:'App',
+        components:{ InnerChild }
+
+    };
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/innerChild.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Hello I'm Inner Child.</h1>
+    <h1>Hello I'm Inner_child component of Child</h1>
+</template>
+<script>
+ export default {
+        name:'App',
+        data(){
+            return {};
+        },
+    };
+</script>
+
+<style>
+
+</style>
